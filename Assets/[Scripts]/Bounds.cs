9e2ee24d9c05264df70d842d516dde86fbab01f3
unknown <selimsahin.sns@gmail.com>
2021-01-14 14:48:23 +0000

    changed 3d surface with planes and implemented better mesh manipulation code

---
+++ Assets/[Scripts]/Bounds.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bounds : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("CylinderPiece")) {
+            Destroy(other.gameObject);
+        }
+    }
+}
---
+++ Assets/[Scripts]/DeformPlane.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeformPlane : MonoBehaviour
+{
+    [SerializeField] float radius;
+    [SerializeField] float power;
+
+    [Tooltip("For expanding the inner circle")]
+    [SerializeField] float scaleFactor = 0f;
+
+    [SerializeField] Transform cylinder;
+
+    private Camera cam;
+    private Ray ray;
+    private RaycastHit hit;
+
+    private MeshFilter meshFilter;
+    private Mesh mesh;
+    Vector3[] vertices;
+
+    private void Start() {
+        cam = Camera.main;
+        meshFilter = GetComponent<MeshFilter>();
+        mesh = meshFilter.mesh;
+        vertices = mesh.vertices;
+    }
+
+    private void FixedUpdate() {
+        ray = cam.ScreenPointToRay(Input.mousePosition);
+        if (Input.GetMouseButton(0) && Physics.Raycast(ray, out hit)) {
+            Deform(hit.point);
+        }
+    }
+
+    public void Deform(Vector3 deformPosition) {
+        deformPosition = transform.InverseTransformPoint(deformPosition);
+
+        for (int i = 0; i < vertices.Length; i++) {
+            float distance = (vertices[i] - deformPosition).sqrMagnitude;
+
+            if (distance < radius) {
+                Vector3 directionFromCenter = (vertices[i] - deformPosition);
+                directionFromCenter.z = 0f;
+                directionFromCenter = directionFromCenter.normalized;
+                vertices[i] += ((Vector3.forward * power) + (directionFromCenter * scaleFactor));
+            }
+        }
+
+        mesh.vertices = vertices;
+        Instantiate(cylinder, new Vector3(hit.point.x, hit.point.y, transform.position.z + 0.5f), Quaternion.Euler(270f, 0f, 0f));
+    }
+}
