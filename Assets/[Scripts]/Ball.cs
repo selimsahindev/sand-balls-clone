1914a0873ded55c9642427662baa5bf610095f70
unknown <selimsahin.sns@gmail.com>
2021-01-15 11:28:26 +0000

    added a truck model, implemented a logic for changing ball's parent to truck when it's in the truck bed

---
+++ Assets/[Scripts]/Ball.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    private Transform parent;
+
+    private void Start() {
+        parent = this.transform.parent;
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("TruckBed")) {
+            transform.parent = other.transform;
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if (other.CompareTag("TruckBed")) {
+            transform.parent = parent;
+        }
+    }
+}
